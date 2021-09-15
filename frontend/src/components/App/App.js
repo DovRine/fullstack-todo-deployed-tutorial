f70132ae0b095626a5e45763c81c340782bc5524
Dov Rine <dov@techimpact.org>
2021-09-15 09:36:40 -0700

    create initial react frontend

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div data-testid="App" className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/App/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import {App} from '..';
+
+describe('App', () => {
+  it('exists', () => {
+    expect(typeof App).toBe('function')
+  })
+  it('has className "App"', () => {
+    render(<App />)
+    const ui = screen.getByTestId('App')
+    expect(ui).toHaveClass('App')
+  })
+})
+
