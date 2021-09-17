9e1ea9bbc03182e267bd6ab70fbdd39fc8f8e894
Dov Rine <dov@techimpact.org>
2021-09-17 16:03:24 -0700

    fix(Todo tests): refactor to use TodoProvider

--- frontend/src/components/Todo/Todo.test.js
+++ frontend/src/components/Todo/Todo.test.js
@@ -1,25 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import { Todo } from "..";
+import TodoProvider from "../../providers/TodoProvider";
+
+function customRender(Component, props, todo) {
+  return render(
+    <TodoProvider values={{ ...props }}>
+      <Component todo={todo} />
+    </TodoProvider>
+  );
+}
 
 describe("Todo", () => {
   it("exists", () => {
     expect(typeof Todo).toBe("function");
   });
   it('has class "Todo"', () => {
-    render(<Todo />);
+    customRender(Todo, {})
     const ui = screen.getByTestId("Todo");
     expect(ui).toHaveClass("Todo");
   });
   it("shows the task of a todo", () => {
     const todo = { id: 1, task: "test task", done: false };
-    render(<Todo todo={todo} />);
+    customRender(Todo, {}, todo)
     const ui = screen.getByTestId("Todo");
     expect(ui).toHaveTextContent(todo.task);
   });
   it("has a delete button", () => {
     const todo = { id: 1, task: "test task", done: false };
     const deleteTodo = jest.fn();
-    render(<Todo todo={todo} deleteTodo={deleteTodo} />);
+    customRender(Todo, {deleteTodo}, todo)
     const btn = screen.getByTestId("BtnDelete");
     btn.click();
     expect(deleteTodo).toHaveBeenCalledTimes(1);
