030bdedaffea27e4aeb7772c8d3071f27d6f95dc
Dov Rine <dov@techimpact.org>
2021-09-17 15:58:54 -0700

    fix(TodoList tests): refactor to use TodoProvider

--- frontend/src/components/TodoList/TodoList.test.js
+++ frontend/src/components/TodoList/TodoList.test.js
@@ -1,32 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import { TodoList } from "..";
+import TodoProvider from "../../providers/TodoProvider";
 
+function customRender(Component, props) {
+  return render(
+    <TodoProvider values={{ ...props }}>
+      <Component />
+    </TodoProvider>
+  );
+}
 describe("TodoList", () => {
   it("exists", () => {
     expect(typeof TodoList).toBe("function");
   });
   it('has class "TodoList"', () => {
-    render(<TodoList deleteTodo={jest.fn()} />);
+    customRender(TodoList, { todos: [] });
     const ui = screen.getByTestId("TodoList");
     expect(ui).toHaveClass("TodoList");
   });
   it("shows an empty list message when no todos", () => {
-    render(<TodoList deleteTodo={jest.fn()} />);
+    customRender(TodoList, { todos: [] });
     screen.getByTestId("EmptyList");
   });
   it("shows a list of todos", () => {
     const todos = [{ id: 1, task: "test task", done: false }];
-    render(<TodoList todos={todos} deleteTodo={jest.fn()} />);
+    customRender(TodoList, { todos });
     const ui = screen.getByTestId("TodoList");
     expect(ui).toHaveTextContent(todos[0].task);
   });
   it("does not show EmptyList when we have todos", () => {
-    render(
-      <TodoList
-        todos={[{ id: 1, task: "test task", done: false }]}
-        deleteTodo={jest.fn()}
-      />
-    );
+    const todos = [{ id: 1, task: "test task", done: false }];
+    customRender(TodoList, { todos });
     const ui = screen.queryByTestId("EmptyList");
     expect(ui).toBeNull();
   });
