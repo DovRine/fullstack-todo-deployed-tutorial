76cf37bd233e19898785fdcde1f6bfa336d32318
Dov Rine <dov@techimpact.org>
2021-09-22 14:32:14 -0700

    implement addTodo

--- backend/src/db.js
+++ backend/src/db.js
@@ -22,3 +22,4 @@ export async function listTodos() {
     throw err;
   }
 }
+
--- backend/src/db.test.js
+++ backend/src/db.test.js
@@ -1,13 +1,34 @@
-import * as db from "./db.js";
+import { jest } from "@jest/globals";
+const mockQuery = jest.fn();
+jest.mock("pg", () => ({
+  Pool: jest.fn(() => ({
+    query: mockQuery,
+  })),
+}));
+
+const db = await import("./db.js");
+const pg = await import("pg");
 
 describe("db", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
   describe("listTodos", () => {
     it("exists", () => {
       expect(typeof db.listTodos).toBe("function");
     });
     it("returns an array of todos", async () => {
+      const testTodos = [
+        { id: 1, task: "buy milk", done: false },
+        { id: 2, task: "walk the dog", done: false },
+      ];
+      mockQuery.mockResolvedValue({
+        rowCount: testTodos.length,
+        rows: testTodos,
+      });
       const todos = await db.listTodos();
-      expect(Array.isArray(todos)).toBe(true);
+      expect(todos).toEqual(testTodos);
+      expect(mockQuery).toHaveBeenCalledTimes(1);
     });
   });
 });
