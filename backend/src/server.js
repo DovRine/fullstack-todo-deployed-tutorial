a6a4304f96563b00f8acefedd82e6262f04c41ae
Dov Rine <dov@techimpact.org>
2021-09-23 16:35:34 -0700

    refactor: split validateTodo from server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,43 +1,5 @@
 import express from "express";
-
-function validateTodo(todo, mode = "add") {
-  if (todo.task === "") {
-    throw new Error("task must not be empty");
-  }
-  if (!todo?.task) {
-    throw new Error("missing key: task");
-  }
-  if (mode === "add") {
-    if (Object.keys(todo).length > 1) {
-      throw new Error("only key: 'task' is allowed");
-    }
-  }
-  if (typeof todo.task !== "string") {
-    throw new Error("only strings are allowed as tasks");
-  }
-  if (mode === "edit" || mode === "delete") {
-    // require {id: int, task: string, done: bool
-    const requiredFields = ["id", "task", "done"];
-    requiredFields.forEach((fld) => {
-      if (!todo.hasOwnProperty("id")) {
-        throw new Error("missing one or more fields of [id, task, done]");
-      }
-    });
-    if (Object.keys(todo).length > 3) {
-      throw new Error("only fields: [id, task, done] are allowed");
-    }
-    if (
-      typeof todo.id !== "number" ||
-      Math.floor(todo.id) !== todo.id ||
-      todo.id < 1
-    ) {
-      throw new Error("id can only be an integer > 0");
-    }
-    if (typeof todo.done !== "boolean") {
-      throw new Error("done must be a boolean");
-    }
-  }
-}
+import validateTodo from "./validateTodo";
 
 export default function makeApp(db) {
   const app = express();
