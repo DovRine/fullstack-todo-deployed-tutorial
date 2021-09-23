42a7f1e556f91afdffba42d0f8163313bb6c4d7c
Dov Rine <dov@techimpact.org>
2021-09-23 10:31:53 -0700

    implement db.addTodo in api

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,15 @@ import express from "express";
 
 export default function makeApp(db) {
   const app = express();
+  app.use(express.json());
 
   app.get("/todos", async (req, res) => {
     res.send(await db.listTodos());
   });
 
+  app.post("/todos", async (req, res) => {
+    const todo = { ...req.body };
+    res.status(201).send(await db.addTodo(todo));
+  });
   return app;
 }
--- backend/src/server.test.js
+++ backend/src/server.test.js
@@ -2,8 +2,11 @@ import { jest } from "@jest/globals";
 import supertest from "supertest";
 import makeApp from "./server.js";
 
+const addTodo = jest.fn();
 const listTodos = jest.fn();
+
 const db = {
+  addTodo,
   listTodos,
 };
 
@@ -33,5 +36,34 @@ describe("server", () => {
       expect(Array.isArray(res.body)).toBe(true);
       expect(listTodos).toHaveBeenCalledTimes(1);
     });
+  });
+
+  describe("POST /todos - add todo", () => {
+    let todo = { task: "buy milk" };
+    async function doAddTodo(returnId=1) {
+      addTodo.mockResolvedValue({ id:  returnId});
+      return await supertest(app)
+        .post("/todos")
+        .set("Accept", "application/json")
+        .send(todo);
+    }
+    it("return content-type json", async () => {
+      const res = await doAddTodo();
+      expect(res.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
+    it("return 201 status code", async () => {
+      const res = await doAddTodo();
+      expect(res.statusCode).toBe(201);
+    });
+    it("return the id of the newly added todo", async () => {
+      const randomId = Math.floor(Math.random() * 100) + 1;
+      const res = await doAddTodo(randomId);
+      expect(res.body).toEqual({ id: randomId });
+      expect(addTodo).toHaveBeenCalledTimes(1);
+      expect(addTodo.mock.calls[0][0]).toEqual(todo);
+    });
+    // TODO: returns status 400 on invalid input
   });
 });
