2e76b32ccc98253d2a9b0d8aa7fecdf4c5a8de75
Dov Rine <dov@techimpact.org>
2021-09-24 12:31:20 -0700

    fix: incorrect sql for add and edit todos

--- backend/src/db.js
+++ backend/src/db.js
@@ -24,7 +24,7 @@ export async function listTodos() {
 }
 
 export async function editTodo(todo) {
-  const sql = "update table todos set task='$1', done=$2 where id=$3";
+  const sql = "update table todos set task=$1, done=$2 where id=$3";
   const params = [todo.task, todo.done, todo.id];
   try {
     await pool.query(sql, params);
@@ -35,7 +35,7 @@ export async function editTodo(todo) {
 }
 
 export async function addTodo(todo) {
-  const sql = "insert into todos (task) values('$1') returning id;";
+  const sql = "insert into todos (task) values($1) returning id;";
   const params = [todo.task];
   try {
     const { rows } = await pool.query(sql, params);
